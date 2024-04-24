44079feac9cef36fcdd28557397c387eb10a1722
LeonelAnchaba <leonelanchaba@gmail.com>
2024-04-23 23:36:27 -0300

    Metodo Post funcionando correctamente. Ya nos lleva al Home

--- frontend/js/formulario.js
+++ frontend/js/formulario.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-
+console.log("GGGGGGGGGGGGGGGG")
     const agregar = document.getElementById("createButton");
   const editar = document.getElementById("editButton")
   const borrar = document.getElementById("deleteButton")
@@ -18,67 +18,65 @@ agregar.addEventListener("click", function(e) {
         length: document.querySelector('#length').value,
     };
 
-    fetch('http://localhost:3031/api/movies/create', {
+    fetch('http://localhost:3031/api/movies/create/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(movie)
     })
-    .then(response => {
-        return response.json()
-    })
+    .then(response => response.json())
     .then(data => {
-        console.log("Que me responde la sda promesa?: ", data);
-        console.log("window", window.location.href.slice(0,10))
+        window.location.href = window.location.href.replace("formulario", "home")
+        // console.log("window...", window.location.href.replace("formulario", "home"))
     })
     
  })
 
 
-editar.addEventListener("click", function (e) {
-    console.log("en evento: ", window.location.pathname.match(/\d+$/)[0])
-    console.log("hiciste click en editar", e)
-    const id = window.location.pathname.match(/\d+$/)[0]
+// editar.addEventListener("click", function (e) {
+//     console.log("en evento: ", window.location.pathname.match(/\d+$/)[0])
+//     console.log("hiciste click en editar", e)
+//     const id = window.location.pathname.match(/\d+$/)[0]
 
-    const movieEdit = {
-        title: document.querySelector('#title').value,
-        rating: document.querySelector('#rating').value,
-        awards: document.querySelector('#awards').value,
-        release_date: document.querySelector('#release_date').value,
-        length: document.querySelector('#length').value,
-    };
+//     const movieEdit = {
+//         title: document.querySelector('#title').value,
+//         rating: document.querySelector('#rating').value,
+//         awards: document.querySelector('#awards').value,
+//         release_date: document.querySelector('#release_date').value,
+//         length: document.querySelector('#length').value,
+//     };
 
-    fetch(`http://localhost:3031/api/movies/update/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(movieEdit)
-    }).then(response => {
-        return response.json()
-    }).then(data =>{
-        console.log("que es data", data)
-    })
+//     fetch(`http://localhost:3031/api/movies/update/${id}`, {
+//         method: 'PUT',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(movieEdit)
+//     }).then(response => {
+//         return response.json()
+//     }).then(data =>{
+//         console.log("que es data", data)
+//     })
 
-})
+// })
 
 
-borrar.addEventListener("click", (e => {
-    console.log("a borrarrrrrrrrrrrrrr")
-    const id = window.location.pathname.match(/\d+$/)[0]
+// borrar.addEventListener("click", (e => {
+//     console.log("a borrarrrrrrrrrrrrrr")
+//     const id = window.location.pathname.match(/\d+$/)[0]
 
-    fetch(`http://localhost:3031/api/movies/delete/${id}`, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json"
-        }
+//     fetch(`http://localhost:3031/api/movies/delete/${id}`, {
+//         method: "DELETE",
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
 
-    }).then(response => {
-        return response.json()
-    }).then(data => {
-        console.log("asdasd", data)
-    })
+//     }).then(response => {
+//         return response.json()
+//     }).then(data => {
+//         console.log("asdasd", data)
+//     })
    
-}))
+// }))
 }
