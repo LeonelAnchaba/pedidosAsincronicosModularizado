96fa3f50c1e76d36ae4be42da76865335a1aa991
LeonelAnchaba <leonelanchaba@gmail.com>
2024-04-24 03:36:10 -0300

    Funciona la sección favoritos. Ya seteo la sessionStorage y en caso de que tenga info aparece el boton para entrar a la sección. Dentro de la sección recupero la información y creo una tarjeta para cada pelicula. Y si borran los registros aparece el mensaje esperado

--- frontend/js/favoritas.js
+++ frontend/js/favoritas.js
@@ -2,16 +2,67 @@ window.onload = () => {
   const app = document.getElementById("root");
   const container = document.createElement("div");
   container.setAttribute("class", "container");
+
+  const trash = document.createElement("i")
+  trash.setAttribute("class", "fa-solid fa-trash")
+  trash.style.position = "absolute"
+  trash.style.right = "20vw"
+  trash.style.top = "20vh"
+
+  app.appendChild(trash)
+
   app.appendChild(container);
 
+  trash.addEventListener("click", (e) => {
+    console.log("tachito")
+    sessionStorage.removeItem("peliculasFavoritas");
+
+    container.style.display = "none"
+
+    const cartel = document.createElement("div")
+  cartel.style.background = "skyblue"
+  cartel.style.border = "solid grey 1px"
+  cartel.style.padding = "10px"
+  cartel.style.margin = "auto"
+  cartel.style.textAlign = "center"
+
+  app.appendChild(cartel)
+
+  const mensaje = document.createElement("h2");
+  mensaje.textContent = "Aún no agregaste peliculas a tu sección favoritos" 
+
+  cartel.appendChild(mensaje)
+
+  })
+
+  
+
   // Aqui debemos agregar nuestro fetch
+  const pelisInfoJSON = sessionStorage.getItem("peliculasFavoritas");
+  console.log("Que llega y en que formato: ", pelisInfoJSON);
+
+  if (pelisInfoJSON) {
+    const pelisInfo = JSON.parse(pelisInfoJSON);
+
+    console.log("hay dataaaaaa", pelisInfoJSON)
+    console.log("parseeeeee", pelisInfo[0])
 
 
 
-  /** Codigo que debemos usar para mostrar los datos en el frontend
-    let data = peliculas.data;
+  for (i = 0; i < pelisInfo.length; i++) {
 
-    data.forEach((movie) => {
+    fetch(`http://localhost:3031/api/movies/${pelisInfo[i]}`)
+  .then(function(response){
+      return response.json()
+  })
+  .then((peliculas)=>{
+
+console.log("promesa2", peliculas.data)
+
+  /** Codigo que debemos usar para mostrar los datos en el frontend **/
+     let movie = peliculas.data;
+
+    // data.forEach((movie) => {
       const card = document.createElement("div");
       card.setAttribute("class", "card");
 
@@ -34,5 +85,16 @@ window.onload = () => {
       }
       card.appendChild(duracion);
     });
-  */
+
+  }
+
+  
+  // })
+
+} else {
+  console.log("no tenes pelis favoritas")
+  
+}
+
+
 };
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,6 +4,9 @@ window.onload = () => {
   container.setAttribute("class", "container");
   app.appendChild(container);
 
+
+  const enlaceAFavoritas = document.querySelector(".buttonFavorites")
+
   // Aqui debemos agregar nuestro fetch
   fetch('http://localhost:3031/api/movies')
   .then(function(response){
@@ -22,18 +25,23 @@ window.onload = () => {
       const card = document.createElement("div");
       card.setAttribute("class", "card");
       card.style.position = "relative";
+      card.style.textAlign = "center"
 
       const star = document.createElement("i");
       star.setAttribute("class", "fa-regular fa-star")
       star.style.position = "absolute"
       star.style.top = "10px"
       star.style.right = "10px"
+      star.setAttribute("id", `${movie.id}`)
 
       const a = document.createElement("a");
       
       a.setAttribute("class", "accesoAlDetalle")
       a.setAttribute("id", `${movie.id}`)
       a.textContent = "Detalle"
+      a.style.background = "skyblue"
+      a.style.padding = "5px 10.5rem"
+      
 
       const h1 = document.createElement("h1");
       h1.textContent = movie.title;
@@ -85,8 +93,45 @@ window.onload = () => {
 
         
        })
+
+
+  })
+})
+
+const buttonFavorites = document.querySelectorAll("i")
+
+buttonFavorites.forEach(star => {
+  star.addEventListener("click", function(e) {
+    enlaceAFavoritas.style.display = "inline"
+    star.style.color = "white"
+    console.log("estrellita dime tu:", star.id)
+
+    const agregarLaPeli = star.id
+    
+
+    let peliculasFavoritas = JSON.parse(sessionStorage.getItem("peliculasFavoritas")) || []
+
+    console.log(sessionStorage)
+
+    peliculasFavoritas.push(agregarLaPeli)
+
+    sessionStorage.setItem("peliculasFavoritas", JSON.stringify(peliculasFavoritas))
+
+    console.log("info del producto en sessionStorage", peliculasFavoritas)
   })
 })
   
 })
+
+
+
+ 
+const pelisInfoJSON = sessionStorage.getItem("peliculasFavoritas");
+console.log("Que llega y en que formato: ", pelisInfoJSON);
+
+if (!pelisInfoJSON) {
+  enlaceAFavoritas.style.display = "none"
 };
+  
+};
+
